644e0a558db212a6e90d1c24f5f87eed6a398e93
veli.adin <veli.adin@mavinci.com.tr>
2022-05-17 13:47:58 +0300

    17.05.2022 13.33

--- src/com/luv2code/springdemo/NotrFortuneService.java
+++ src/com/luv2code/springdemo/NotrFortuneService.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+@Component
 public class NotrFortuneService implements FortuneService {
     @Override
     public String getFortune() throws IOException {
