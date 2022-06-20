51375d8b9a2664de59ea4cff9dfa456c0e38f54c
halilibrahimozturk <halilibraahimozturk@gmail.com>
2022-06-20 11:05:10 +0300

    20.06.2022 11.05

--- src/com/halilibrahimozturk/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/halilibrahimozturk/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,7 +11,7 @@ public class MyDemoLoggingAspect {
 
 
     //this is where we add all of our related advices for logging
-
+    //
     //lets start with an @Before advice
 
     @Before("execution(public void addAccount())")
